aea6296a2ae66ed98a87cd4074936ac979d5dc38
ThomasPhilibert0 <43186470+ThomasPhilibert0@users.noreply.github.com>
2019-01-23 22:59:46 +0100

    Add files via upload

--- TP1 UPICI/tp1/src/Q7.py
+++ TP1 UPICI/tp1/src/Q7.py
@@ -6,7 +6,7 @@ import equations
 
 
 def EQDiff() :
-    """Résolution approchée de y' = A*y avec y(0) = (1,1) pour t dans [0,1] avec la méthode d'Euler"""
+    """Résolution approchée de y' = A*y avec y(0) = (1,1) pour t dans [0,1] """
 
     A = np.diag([-1,-2])
     t0,y0 = 0.,np.array([1,1])
@@ -15,8 +15,10 @@ def EQDiff() :
     
     
     N = int(T/h)  
-    [t,y1] = methodesQ7.saute_mouton(t0,h,N,y0,equations.f_diff,2,A)
- 
+    [t,y1] = methodesQ7.euler_explicite(t0,h,N,y0,equations.f_diff,2,A)
+    [t,y2] = methodesQ7.RK2(t0,h,N,y0,equations.f_diff,2,A)
+    [t,y3] = methodesQ7.saute_mouton(t0,h,N,y0,equations.f_diff,2,A)
+    [t,y4] = methodesQ7.trapezePC(t0,h,N,y0,equations.f_diff,2,A)
     
     
-    return y1
+    return y1,y2,y3,y4
