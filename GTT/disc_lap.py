7ec4bd3fc771570f87928781e101c8f8af81b7a1
mnastorg <matthieunastorg@gmail.com>
2019-01-31 00:46:46 +0100

    Une avancée dans le tp

---
+++ GTT/disc_lap.py
@@ -0,0 +1,93 @@
+import numpy as np
+import scipy.sparse as sparse   # Algèbre linéaire creuse
+import matplotlib.pyplot as plt # Pour les graphiques
+from scipy.sparse.linalg import spsolve
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def matrix_lap(N):
+    """Retourne une matrice qui discrétise le laplacien de u dans le domaine Omega = [xmin,xmax,ymin,ymax], découpé en N intervalles en x et y. La matrice finale est une matrice scipy.sparse CSR matrix. Cette matrice est de taille (N+1)*(N+1)"""
+
+    h = 1./N
+    h2 = h*h
+
+    #On note les inconnues de 0 à Nx suivant x et 0 à Ny suivant y. La taille du problème est donc (Nx+1)*(Ny+1).
+
+    #Cela correspond à x_i = i*h et y_j = j*h et la numérotation (i,j) --> k := (N+1)*j+i.
+
+    taille = (1+N)*(1+N)
+
+    diags = np.zeros((5,taille))
+
+    #Diagonale principale
+    diags[2,:] = 1.
+    diags[2,np.arange(N+2, taille - (N+2),N+1)] = -4./h2
+    diags[2,np.arange(N+3, taille - (N+1),N+1)] = -4./h2
+
+    #Diagonale "-1"
+    diags[1,np.arange(N+1, taille -(N+2),N+1)] = 1./h2
+    diags[1,np.arange(N+2, taille -(N+1),N+1)] = 1./h2
+    
+    #Diagonale "+1"
+    diags[3,np.arange(N+3, taille -(N+1),N+1)] = 1./h2
+    diags[3,np.arange(N+4, taille -N,N+1)] = 1./h2
+
+    #Diagonale "-(N+1)"
+    diags[0,np.arange(1,taille -2*N+3,N+1)] = 1./h2
+    diags[0,np.arange(2,taille -2*(N+1),N+1)] = 1./h2
+
+    #Diagonale "+(N+1)"
+    diags[4,np.arange(2*N+3,taille -1,N+1)] = 1./h2
+    diags[4,np.arange(2*N+4,taille ,N+1)] = 1./h2
+
+    #Construction de la matrice creuse
+    A = sparse.spdiags(diags,[-(N+1),-1,0,1,(N+1)],taille,taille, format = "csr")
+
+    return A
+    
+def f(x1,x2):
+    return 6.*(1.-3.*x1+2.*x1**2)*((x2-1.)**3)*x2 + 6.*(1.-3.*x2+2.*x2**2)*((x1-1.)**3)*x1
+
+def u(x1,x2):
+    return x1*x2*((x1-1.)**3)*((x2-1))**3
+
+def sol_disc(N):
+    x = np.linspace(0,1,N+1)
+    y = np.linspace(0,1,N+1)
+
+    F = np.zeros((N+1)*(N+1))   #Allocation mémoire de f
+    V = np.zeros((N+1)*(N+1))   #Allocation mémoire sol exacte
+
+    for i in np.arange(N+1):
+        for j in np.arange(N+1):
+            k = i + j*(N+1)
+            F[k] = -f(x[i],y[i])
+            V[k] = u(x[i],y[i])
+
+    U = np.zeros((N+1)*(N+1)) #matrice pour la solution
+    A = matrix_lap(N)
+
+    #plt.spy(A)
+    #plt.show()
+        
+    U = spsolve(A,F)
+
+    fig = plt.figure(1)
+    ax = Axes3D(fig)
+    X,Y = np.meshgrid(x,y)
+    ax.plot_surface(X,Y, U.reshape((N+1,N+1)), cmap='hot')
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("Solution discrétisée")
+    
+    fig2 = plt.figure(2)
+    ax = Axes3D(fig2)
+    X,Y = np.meshgrid(x,y)
+    ax.plot_surface(X,Y,V.reshape((N+1,N+1)),cmap = 'hot')
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("Solution exacte")
+    
+    plt.show()
+
+
