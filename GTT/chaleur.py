a6cddca3408518e58a539fb42ec1971a9a9633b5
Matthieu Nastorg <matthieunastorg@gmail.com>
2019-02-05 12:40:28 +0100

    Chaleur

---
+++ GTT/chaleur.py
@@ -0,0 +1,74 @@
+import numpy as np
+import scipy.sparse as sparse   # Algèbre linéaire creuse
+import matplotlib.pyplot as plt # Pour les graphiques
+import scipy.sparse.linalg as sci
+from mpl_toolkits.mplot3d import Axes3D
+
+def matrix_lap(N,dt):
+    """Retourne une matrice qui discrétise le laplacien de u dans le domaine Omega = [xmin,xmax,ymin,ymax], découpé en N intervalles en x et y. La matrice finale est une matrice scipy.sparse CSR matrix. Cette matrice est de taille (N+1)*(N+1)"""
+
+    h = 1./N
+    h2 = h*h
+
+    #On note les inconnues de 0 à Nx suivant x et 0 à Ny suivant y. La taille du problème est donc (Nx+1)*(Ny+1).
+
+    #Cela correspond à x_i = i*h et y_j = j*h et la numérotation (i,j) --> k := (N+1)*j+i.
+
+    taille = (1+N)*(1+N)
+
+    diags = np.zeros((5,taille))
+
+    #Diagonale principale
+    diags[2,:] = 1.
+    diags[2, N+2:taille - (N+2)] = -(4*dt)/h2
+    diags[2, np.arange(2*N+1, taille, N+1)] = 1.
+    diags[2, np.arange(2*N+2, taille, N+1)] = 1.
+              
+    #Diagonale "-1"
+    diags[1,N+1:taille-(N+1)] = 1./h2
+    diags[1, np.arange(2*N, taille, N+1)] = 0.
+    diags[1, np.arange(2*N+1, taille, N+1)] = 0.
+    
+    #Diagonale "+1"
+    diags[3, N+3:taille-(N+1)] = 1./h2
+    diags[3, np.arange(2*N+2, taille, N+1)] = 0.
+    diags[3, np.arange(2*N+3, taille, N+1)] = 0.
+
+    #Diagonale "-(N+1)"
+    diags[0, 1 : taille - (2*N+3)] = 1./h2
+    diags[0, np.arange(N,taille,N+1)] = 0.
+    diags[0, np.arange(N+1,taille,N+1)] = 0.
+
+    #Diagonale "+(N+1)"
+    diags[4, taille - N*N + 2 : taille - 1] = 1./h2
+    diags[4, np.arange(taille - N*N + 1 + N ,taille,N+1)] = 0.
+    diags[4, np.arange(taille - N*N + 2 + N ,taille,N+1)] = 0.
+
+    #Construction de la matrice creuse
+    A = sparse.spdiags(diags,[-(N+1),-1,0,1,(N+1)],taille,taille, format = "csr")
+
+    return A
+
+def f(x1,x2):
+    return 6.*(1.-3.*x1+2.*x1**2)*((x2-1.)**3)*x2 + 6.*(1.-3.*x2+2.*x2**2)*((x1-1.)**3)*x1
+
+def chaleur (N,dt,T):
+    x = np.linspace(0,1,N+1)
+    y = np.linspace(0,1,N+1)
+
+    taille = (N+1)*(N+1)
+
+    F = np.zeros(taille)      #Allocation mémoire de f
+    I = sparse.eye(taille)
+    temps = np.zeros(T)       #Allocation tableau des temps
+
+    for i in range (1,N):
+        for j in range (1,N):
+            k = i + j*(N+1)
+            F[k] = f(x[i],y[j])
+
+    for t in range (0,T):
+       C = I -  matrix_lap(N,dt)
+       temps[t+1] = sci.spsolve(C,(temps[t]- F))
+
+    return temps  
