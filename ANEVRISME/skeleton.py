f9a4527e7d8dde2577cd2166cadfdba40c605c42
mnastorg <matthieunastorg@gmail.com>
2019-09-18 20:32:03 +0200

    Debut

--- ANEVRISME/skeleton.py
+++ ANEVRISME/skeleton.py
@@ -15,14 +15,38 @@ def skeleton(N) :
     x = np.linspace(0,1,N+1)
     y = np.linspace(0,1,N+1)
 
-    SKELET = np.loadtxt("penal_30_ferme")
+    dir1 = 0
+    dir2 = 0
+    dir2 = 0
+    dir3 = 0
+    dir4 = 0
 
-    for j in range(N+1):
-        MAX = max(SKELET[j,:])
-        for i in range(N+1):
-            if SKELET[j][i] != MAX :
-                SKELET[j][i] = 0
-                
+    MAT = np.loadtxt("penal_20_6")
+    SKELET = np.zeros((N+1,N+1))
+    DIR = 0
+
+    for i in range(1,N):
+        for j in range(1,N):
+            pix = MAT[j][i]
+            #Première direction(verticale)
+            if pix > MAT[j-1][i] and pix > MAT[j+1][i]:
+                dir1 = 1
+                DIR = dir1
+            #Seconde direction(horizontale)
+            if pix > MAT[j][i+1] and pix > MAT[j][i-1]:
+                dir2 = 1
+                DIR = DIR + dir2
+            #Troisieme direction(diagonale g_d)
+            if pix > MAT[j-1][i+1] and pix > MAT[j+1][i-1]:
+                dir3 = 1
+                DIR = DIR + dir3
+            #Quatrième direction(diagonale d_g)
+            if pix > MAT[j-1][i-1] and pix > MAT[j+1][i+1]:
+                dir4 = 1
+                DIR = DIR + dir4
+
+        SKELET[j][i] = DIR
+            
     fig = plt.figure(figsize = plt.figaspect(0.35))
 
     ax = fig.add_subplot(111, projection = '3d')
